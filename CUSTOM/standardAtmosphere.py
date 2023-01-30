04fa70bf23861a0ce1095ab86392a8c64d38bcce
sladesladeslade <spbrooks4@gmail.com>
2023-01-29 22:59:38 -0500

    map of file

---
+++ CUSTOM/standardAtmosphere.py
@@ -0,0 +1,57 @@
+# Brooks Aerospace Design Suite VSP API Commands
+# Slade Brooks
+# spbrooks4@gmail.com
+# i stole dis from class
+
+"""
+This code is a class called standardAtmosphere. It performs calculations
+and returns standard atmosphere values at a specific altitude. Each
+atmospheric property is its own function within the class, and there is
+an SI conversion function as well. Each function returns the value at the
+requested altitude and a separate string for units.
+
+It includes the properties:
+
+"""
+
+class standardAtmosphere():
+    """
+    This class contains all of the following standard atmosphere
+    properties at a requested altitude and a unit conversion function.
+    """
+
+    def tempF():
+        """
+        This function returns the temperature in deg F
+        of a US standard day at the requested altitude.
+
+        :param alt: float, altitude in feet
+        :returns tempF, units: temp in deg F
+        :rtype: float
+        :returns units: units of value
+        :rtype: string
+        """
+
+    def tempR():
+
+    def TR():
+
+    def PR():
+
+    def pres():
+
+    def rho():
+
+    def DR():
+
+    def sqrtDR():
+
+    def QMS():
+
+    def SPW():
+
+    def Aspeed():
+
+    def velA():
+
+    def VRkin():
