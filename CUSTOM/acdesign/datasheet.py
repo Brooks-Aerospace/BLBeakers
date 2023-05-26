07540fc24dc83a6d79bd443eaa105d4df1f59175
sladesladeslade <spbrooks4@gmail.com>
2023-05-26 16:27:38 -0400

    added ac design files

---
+++ CUSTOM/acdesign/datasheet.py
@@ -0,0 +1,14 @@
+"""
+# Aircraft Datasheet Class
+# Slade Brooks
+# spbrooks4@gmail.com
+
+This code is a class called acDatasheet. It takes inputs of
+various aircraft information and saves it to be accessed for
+performance calculations our outputting to other software.
+
+It includes:
+    surface sizes, dimensions, and locations
+    lift and drag info
+    max q/M
+"""
