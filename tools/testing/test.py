00d9e788209ceb3e71915dc37e6002c66ef1dc1c
sladesladeslade <spbrooks4@gmail.com>
2023-06-24 20:26:21 -0400

    [test] Update file a lil bit

--- tools/testing/test.py
+++ tools/testing/test.py
@@ -8,14 +8,19 @@ import design.weights.weights as wt
 import matplotlib.pyplot as plt
 
 
+# create and test wing planform and drag
 wing = wing.wing()
-print(wing.planform(714.3, 8, .35, 31.5))
-print(wing.drag(0.82, 36000, 0.12, 0.4, 0.3171, 0.8, -1.33))
+# print(wing.planform(714.3, 8, .35, 31.5))
+# print(wing.drag(0.82, 36000, 0.12, 0.4, 0.3171, 0.8, -1.33))
 # plt.show()
 
+# test itertow
 itertow = wt.weights()
 itertows = itertow.itertow(.82, 3800, 36000, 2600, 0.65, 8, 0.6, 45, 5, 1)
-print(itertows)
+# print(itertows)
 
+# test wingload
 WSs = wing.wingload(itertows)
-print(WSs)
+# print(WSs)
+
+# TODO: test groundroll
