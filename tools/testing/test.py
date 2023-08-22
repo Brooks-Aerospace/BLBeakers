09b9c69b3e41bada6c6b295fc645df3ff36dd847
sladesladeslade <spbrooks4@gmail.com>
2023-08-22 11:38:02 -0400

    [tools] Add testing of fuse drag estimation

--- tools/testing/test.py
+++ tools/testing/test.py
@@ -16,7 +16,6 @@ wing.planform(714.3, 8, .35, 31.5)
 # print(wing.planform(714.3, 8, .35, 31.5))
 # cruiseCL = wing.cruiseCL(5, 0.8)
 # print(wing.drag(0.82, 36000, 0.12, 0.4, cruiseCL, -1.33))
-# plt.show()
 
 # test itertow
 itertow = wt.weights()
@@ -24,15 +23,20 @@ itertows = itertow.itertow(.82, 3800, 36000, 2600, 0.65, 8, 0.6, 45, 5, 1)
 # print(itertows)
 
 # test wingload
-WSs = wing.wingload(itertows)
+WSs, _ = wing.wingload(itertows)
 # print(WSs[0])
 
 # test groundroll
-ToL = np.array(wing.groundroll(WSs[0], 0.3548, [1.89, 2.1], 0))
+ToL = np.array(wing.groundroll(WSs, 0.3548, [1.89, 2.1], 0))
 # print(ToL)
 
 # test psc gen
 fuse = fuse.fuse()
 plt.figure()
-fuse.PSCylGen(7, 11.5, [0.4, 0.2], 0.6)
-plt.show()
+PSCyl = fuse.PSCylGen(7, 11.5, [0.3, 0.2], 0.6)
+Swet, Drag, Cd0 = fuse.PSCylDrag(PSCyl, 0.82, 37500, 714.3)
+print(Swet)
+print(Drag)
+print(Cd0)
+
+# plt.show()
